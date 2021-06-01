7a888bbdbc9d9b6ecc99e57add5653a0004de46c
armaniferrante <armaniferrante@gmail.com>
2021-06-01 15:42:03 -0700

    Add close/init

--- packages/swap/src/idl.ts
+++ packages/swap/src/idl.ts
@@ -2,376 +2,438 @@ import { Idl } from '@project-serum/anchor';
 
 // Idl for client generation.
 export const IDL: Idl = {
-  version: '0.0.0',
-  name: 'swap',
-  instructions: [
+  "version": "0.0.0",
+  "name": "swap",
+  "instructions": [
     {
-      name: 'swap',
-      accounts: [
+      "name": "initAccount",
+      "accounts": [
         {
-          name: 'market',
-          accounts: [
-            {
-              name: 'market',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'openOrders',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'requestQueue',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'eventQueue',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'bids',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'asks',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'orderPayerTokenAccount',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'coinVault',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'pcVault',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'vaultSigner',
-              isMut: false,
-              isSigner: false,
-            },
-            {
-              name: 'coinWallet',
-              isMut: true,
-              isSigner: false,
-            },
-          ],
+          "name": "openOrders",
+          "isMut": true,
+          "isSigner": false
         },
         {
-          name: 'authority',
-          isMut: false,
-          isSigner: true,
+          "name": "authority",
+          "isMut": false,
+          "isSigner": true
         },
         {
-          name: 'pcWallet',
-          isMut: true,
-          isSigner: false,
+          "name": "market",
+          "isMut": false,
+          "isSigner": false
         },
         {
-          name: 'dexProgram',
-          isMut: false,
-          isSigner: false,
+          "name": "dexProgram",
+          "isMut": false,
+          "isSigner": false
         },
         {
-          name: 'tokenProgram',
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: 'rent',
-          isMut: false,
-          isSigner: false,
-        },
+          "name": "rent",
+          "isMut": false,
+          "isSigner": false
+        }
       ],
-      args: [
-        {
-          name: 'side',
-          type: {
-            defined: 'Side',
-          },
-        },
-        {
-          name: 'amount',
-          type: 'u64',
-        },
-        {
-          name: 'minExchangeRate',
-          type: {
-            defined: 'ExchangeRate',
-          },
-        },
-      ],
+      "args": []
     },
     {
-      name: 'swapTransitive',
-      accounts: [
+      "name": "closeAccount",
+      "accounts": [
         {
-          name: 'from',
-          accounts: [
-            {
-              name: 'market',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'openOrders',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'requestQueue',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'eventQueue',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'bids',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'asks',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'orderPayerTokenAccount',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'coinVault',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'pcVault',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'vaultSigner',
-              isMut: false,
-              isSigner: false,
-            },
-            {
-              name: 'coinWallet',
-              isMut: true,
-              isSigner: false,
-            },
-          ],
+          "name": "openOrders",
+          "isMut": true,
+          "isSigner": false
         },
         {
-          name: 'to',
-          accounts: [
-            {
-              name: 'market',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'openOrders',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'requestQueue',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'eventQueue',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'bids',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'asks',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'orderPayerTokenAccount',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'coinVault',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'pcVault',
-              isMut: true,
-              isSigner: false,
-            },
-            {
-              name: 'vaultSigner',
-              isMut: false,
-              isSigner: false,
-            },
-            {
-              name: 'coinWallet',
-              isMut: true,
-              isSigner: false,
-            },
-          ],
+          "name": "authority",
+          "isMut": false,
+          "isSigner": true
         },
         {
-          name: 'authority',
-          isMut: false,
-          isSigner: true,
+          "name": "destination",
+          "isMut": true,
+          "isSigner": false
         },
         {
-          name: 'pcWallet',
-          isMut: true,
-          isSigner: false,
+          "name": "market",
+          "isMut": false,
+          "isSigner": false
         },
         {
-          name: 'dexProgram',
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: 'tokenProgram',
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: 'rent',
-          isMut: false,
-          isSigner: false,
-        },
+          "name": "dexProgram",
+          "isMut": false,
+          "isSigner": false
+        }
       ],
-      args: [
-        {
-          name: 'amount',
-          type: 'u64',
-        },
-        {
-          name: 'minExchangeRate',
-          type: {
-            defined: 'ExchangeRate',
-          },
-        },
-      ],
+      "args": []
     },
+    {
+      "name": "swap",
+      "accounts": [
+        {
+          "name": "market",
+          "accounts": [
+            {
+              "name": "market",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "openOrders",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "requestQueue",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "eventQueue",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "bids",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "asks",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "orderPayerTokenAccount",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "coinVault",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "pcVault",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "vaultSigner",
+              "isMut": false,
+              "isSigner": false
+            },
+            {
+              "name": "coinWallet",
+              "isMut": true,
+              "isSigner": false
+            }
+          ]
+        },
+        {
+          "name": "authority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "pcWallet",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "dexProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "rent",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "side",
+          "type": {
+            "defined": "Side"
+          }
+        },
+        {
+          "name": "amount",
+          "type": "u64"
+        },
+        {
+          "name": "minExchangeRate",
+          "type": {
+            "defined": "ExchangeRate"
+          }
+        }
+      ]
+    },
+    {
+      "name": "swapTransitive",
+      "accounts": [
+        {
+          "name": "from",
+          "accounts": [
+            {
+              "name": "market",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "openOrders",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "requestQueue",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "eventQueue",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "bids",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "asks",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "orderPayerTokenAccount",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "coinVault",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "pcVault",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "vaultSigner",
+              "isMut": false,
+              "isSigner": false
+            },
+            {
+              "name": "coinWallet",
+              "isMut": true,
+              "isSigner": false
+            }
+          ]
+        },
+        {
+          "name": "to",
+          "accounts": [
+            {
+              "name": "market",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "openOrders",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "requestQueue",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "eventQueue",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "bids",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "asks",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "orderPayerTokenAccount",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "coinVault",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "pcVault",
+              "isMut": true,
+              "isSigner": false
+            },
+            {
+              "name": "vaultSigner",
+              "isMut": false,
+              "isSigner": false
+            },
+            {
+              "name": "coinWallet",
+              "isMut": true,
+              "isSigner": false
+            }
+          ]
+        },
+        {
+          "name": "authority",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "pcWallet",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "dexProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "rent",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "amount",
+          "type": "u64"
+        },
+        {
+          "name": "minExchangeRate",
+          "type": {
+            "defined": "ExchangeRate"
+          }
+        }
+      ]
+    }
   ],
-  types: [
+  "types": [
     {
-      name: 'Side',
-      type: {
-        kind: 'enum',
-        variants: [
+      "name": "Side",
+      "type": {
+        "kind": "enum",
+        "variants": [
           {
-            name: 'Bid',
+            "name": "Bid"
           },
           {
-            name: 'Ask',
-          },
-        ],
-      },
+            "name": "Ask"
+          }
+        ]
+      }
     },
     {
-      name: 'ExchangeRate',
-      type: {
-        kind: 'struct',
-        fields: [
+      "name": "ExchangeRate",
+      "type": {
+        "kind": "struct",
+        "fields": [
           {
-            name: 'rate',
-            type: 'u64',
+            "name": "rate",
+            "type": "u64"
           },
           {
-            name: 'decimals',
-            type: 'u8',
-          },
-        ],
-      },
-    },
+            "name": "decimals",
+            "type": "u8"
+          }
+        ]
+      }
+    }
   ],
-  events: [
+  "events": [
     {
-      name: 'DidSwap',
-      fields: [
+      "name": "DidSwap",
+      "fields": [
         {
-          name: 'given_amount',
-          type: 'u64',
-          index: false,
+          "name": "given_amount",
+          "type": "u64",
+          "index": false
         },
         {
-          name: 'min_exchange_rate',
-          type: {
-            defined: 'ExchangeRate',
+          "name": "min_exchange_rate",
+          "type": {
+            "defined": "ExchangeRate"
           },
-          index: false,
+          "index": false
         },
         {
-          name: 'from_amount',
-          type: 'u64',
-          index: false,
+          "name": "from_amount",
+          "type": "u64",
+          "index": false
         },
         {
-          name: 'to_amount',
-          type: 'u64',
-          index: false,
+          "name": "to_amount",
+          "type": "u64",
+          "index": false
         },
         {
-          name: 'spill_amount',
-          type: 'u64',
-          index: false,
+          "name": "spill_amount",
+          "type": "u64",
+          "index": false
         },
         {
-          name: 'from_mint',
-          type: 'publicKey',
-          index: false,
+          "name": "from_mint",
+          "type": "publicKey",
+          "index": false
         },
         {
-          name: 'to_mint',
-          type: 'publicKey',
-          index: false,
+          "name": "to_mint",
+          "type": "publicKey",
+          "index": false
         },
         {
-          name: 'quote_mint',
-          type: 'publicKey',
-          index: false,
+          "name": "quote_mint",
+          "type": "publicKey",
+          "index": false
         },
         {
-          name: 'authority',
-          type: 'publicKey',
-          index: false,
-        },
-      ],
-    },
+          "name": "authority",
+          "type": "publicKey",
+          "index": false
+        }
+      ]
+    }
   ],
-  errors: [
+  "errors": [
     {
-      code: 100,
-      name: 'SwapTokensCannotMatch',
-      msg: 'The tokens being swapped must have different mints',
+      "code": 100,
+      "name": "SwapTokensCannotMatch",
+      "msg": "The tokens being swapped must have different mints"
     },
     {
-      code: 101,
-      name: 'SlippageExceeded',
-      msg: 'Slippage tolerance exceeded',
-    },
-  ],
+      "code": 101,
+      "name": "SlippageExceeded",
+      "msg": "Slippage tolerance exceeded"
+    }
+  ]
 };
